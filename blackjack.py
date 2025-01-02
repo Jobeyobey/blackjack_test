f4f0e7d06c73e9aa557a3a3fcc98d0da7b3d509b
jobeyobey <hollands.joe@gmail.com>
2025-01-02 18:50:58 +0000

    cleanup

--- blackjack.py
+++ blackjack.py
@@ -2,8 +2,8 @@ from src.deck import Deck
 
 
 def play():
-    print('Hello, potential future BBC developer!')  # execution starts here! remove this print() and add your own code.
+    pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     play()
